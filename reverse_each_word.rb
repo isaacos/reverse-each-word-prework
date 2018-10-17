a97d966ffe14099d02dd7e1497faa38e210056be
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-17 12:54:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (string)
   array = string.split(' ')
   reversed_array = []
   
-  array.collect.collect do |word|
+  array.collect do |word|
     reversed_array.push( word.reverse)
   end 
   reversed_array.join(' ')
